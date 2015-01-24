a7c200b03829b1495711a3fe1f3b25438f3d1eee
Jean Rodrigues <jean.rodrigues@gmail.com>
2015-01-24 15:53:02 -0200

    RChart addition

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(shiny)
+require(rCharts)
 
 shinyServer(function(input, output) {
   
@@ -19,10 +20,19 @@ shinyServer(function(input, output) {
       need(nchar(input$name)>0, "Please input a valid name.")
     )
     
-    data <- paste0("Hi, ", inputVarSex(), ". You are ", input$age, " years old.") 
+    data <- paste0("Hi, ", inputVarSex(), ". You are ", input$age, " years old. Your hair is ", tolower(c(input$hair)), " and eyes are ", tolower(c(input$eye)), ".") 
   })
 
-  
+  output$myChart <- renderChart2({
+    hair_eye <- as.data.frame(HairEyeColor)
+    p1 <- nPlot(Freq ~ Hair, group='Eye', data = subset(hair_eye, Sex == input$sex & Hair==input$hair), 
+                   type = 'multiBarChart', id = 'myChart', width = 800)
+    p1$yAxis(axisLabel = "Frequencies", width = 20, margin = list(left = 50))
+    p1$xAxis(axisLabel = paste("Hair for", input$sex))
+    
+    p1$chart(color = c('brown', 'blue', '#594c25', 'green'))
+    return(p1)
+  })
   output$nameText <- renderText({inputVarData()})    
   
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,27 @@
 library(shiny)
+require(rCharts)
+#options(RCHART_LIB = 'nvd3')
+
 
 shinyUI(fluidPage(
   titlePanel("Simple Shiny App"),
   
-#   sidebarLayout(
-#     sidebarPanel(
-#       p("Shiny is available on CRAN, just install it:"),
-#       code('install.packages("shiny")')
-#     ),
     mainPanel(
-      h3("Introduction"),
+      h3("Introduction Info"),
       p("This simple App will ask for some information about you and do some simple text manipulation with the data provided."),
       p("No text will be diplayed until you provide a name. If the text box becomes red, you probably made a mistake. Age must be a number between 5 and 120."),
-      p(),
+      p("This app will show a bar chart from HairEyeColor data set filtered by your hair and sex info."),
       textInput("name", label = "Tell us your name:"),
       selectInput("sex", label = "Your sex:", choices = c("Male", "Female")),
+      selectInput("eye", label = "Your eye color:", choices = c("Blue", "Brown", "Green", "Hazel")),
+      selectInput("hair", label = "Your hair color:", choices = c("Black", "Blond", "Brown", "Red")),
       numericInput("age", label = "Age:", min = 5, max = 120, value=5),
       br(),
       submitButton("Submit"),      
       br(),
-      p("Processed data: ", verbatimTextOutput("nameText"))
-#     )
+      p("Processed data: ", verbatimTextOutput("nameText")),
+      p("Below, a bar chart showing distribution of eye colors versus hair colors accordingly to sex and hair type provided (from HairEyeColor dataset)"),
+      showOutput("myChart", "nvd3"),
+      p("You can click on the chart to perform different arrangements.")
   )
 ))
